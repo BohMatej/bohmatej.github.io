9d759f763c0e25374776eabcb5f6bfa7871c124f
Matej Macula <macula.m@northeastern.edu>
2026-03-03 11:53:51 +0000

    migration attempt

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
+const repo = 'personal-website'; // e.g. personal-site
+const isProjectPage = false; // false if repo is username.github.io
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: isProjectPage ? `/${repo}` : '',
+  assetPrefix: isProjectPage ? `/${repo}/` : '',
 };
 
 export default nextConfig;
